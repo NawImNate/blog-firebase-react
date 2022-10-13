a2e863561ead37395564737421a3a667eb6547f6
NawImNate <jnhatch3@gmail.com>
2022-10-13 16:46:27 -0400

    successfully connecting to mongodb cluster

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from "express";
-const port = 5001;
+import mongoose from "mongoose";
 const app = express();
+const port = 5001;
 
 // middlewares
 app.use("/api", (req, res) => {
@@ -8,4 +9,10 @@ app.use("/api", (req, res) => {
   console.log(`app listening on port ${port}`);
 });
 
-app.listen(port);
+mongoose
+  .connect(
+    "mongodb+srv://NawImNate:U1jfPMZCuT8lpd8C@natesmernblogcluster.jl1jahv.mongodb.net/NatesMernBlogCluster?retryWrites=true&w=majority"
+  )
+  .then(() => app.listen(port))
+  .then(() => console.log(`connected to db, listening to localhost ${port}`))
+  .catch((err) => console.log(err));
