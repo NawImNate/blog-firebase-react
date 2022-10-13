3395613e1c9d40a8163e54dde972420b46697e91
NawImNate <jnhatch3@gmail.com>
2022-10-13 16:21:30 -0400

    configured app file, server runs successfully

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 import express from "express";
-
+const port = 5001;
 const app = express();
 
 // middlewares
-app.use("/api", (req, res, next) => {
+app.use("/api", (req, res) => {
   res.send("Hi");
+  console.log(`app listening on port ${port}`);
 });
 
-app.listen(5000);
+app.listen(port);
