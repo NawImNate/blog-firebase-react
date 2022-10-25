02f589110c9371561e72ee54a0603eacbc726e9b
NawImNate <jnhatch3@gmail.com>
2022-10-25 14:36:37 -0400

    signup controller function with validation

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -12,3 +12,31 @@ export const getAllUser = async (req, res, next) => {
   }
   return res.status(200).json({ users });
 };
+
+export const signup = async (req, res, next) => {
+  // define req body
+  const { name, email, password } = req.body;
+  // define variable for validation
+  let existingUser;
+  // search for user using mongoose findOne
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (err) {
+    return console.log(err.message);
+  }
+  if (existingUser) {
+    return res.status(400).json({ message: "User Exists, please login" });
+  }
+  // if no existing user save a new user and return json or err msg if not successful.
+  const user = new User({
+    name,
+    email,
+    password,
+  });
+  try {
+    await user.save();
+  } catch (error) {
+    return console.log(error.message);
+  }
+  return res.status(201).json({ user });
+};
