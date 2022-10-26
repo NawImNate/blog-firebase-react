c4ff434126c8f512e698d729e7a4e7295564fcde
NawImNate <jnhatch3@gmail.com>
2022-10-26 18:06:41 -0400

    added login post req w/ route and controller fn

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getAllUser, signup } from "../controllers/user-controller";
+import { getAllUser, signup, login } from "../controllers/user-controller";
 
 const router = express.Router();
 
 // Requests, define route as first param, and controller function as second param
 router.get("/", getAllUser);
 router.post("/signup", signup);
+router.post("/login", login);
 
 export default router;
