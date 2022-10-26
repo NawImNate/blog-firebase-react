e760e1187b709128f74418b5ba4a822fa96832b1
NawImNate <jnhatch3@gmail.com>
2022-10-26 18:07:17 -0400

    added login fn, validation for email & pw

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -42,3 +42,23 @@ export const signup = async (req, res, next) => {
   }
   return res.status(201).json({ user });
 };
+
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  let existingUser;
+  // search for user using mongoose findOne
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (err) {
+    return console.log(err.message);
+  }
+  if (!existingUser) {
+    return res.status(404).json({ message: "Couldnt Find User by this email" });
+  }
+
+  const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+  if (!isPasswordCorrect) {
+    return res.status(404).json({ message: "Incorrect password" });
+  }
+  return res.status(200).json({ message: "Login Successful" });
+};
